20b29d65ab12c4f215eb29efac179e1562a6b371
FunkenFlake <kustsoul@gmail.com>
2022-09-04 19:17:59 +0300

    0409

--- NyetHack/src/test/kotlin/TavernTest166.kt
+++ NyetHack/src/test/kotlin/TavernTest166.kt
@@ -1,16 +1,22 @@
 import kotlin.math.roundToInt
 const val TAVERN_NAME = "Taernyl's Folly"
+const val PINTA = 0.125
 
 var playerGold = 10
 var playerSilver = 10
+var barrel = 5.0
 
 fun main() {
+    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("shandy,Dragon's Breath,5.91")
     placeOrder("shandy,Dragon's Breath,5.91")
 }
 
 // Функция совершения покупки
 fun performPurchase(price: Double) {
     displayBalance() // показываем начальный баланс
+    displayRemainderBarrel(barrel) // показываем начальный остаток в бочке
 
     // преобразуем золото и серебро в одну валюту
     val totalPurse = playerGold + (playerSilver / 100.0)
@@ -20,6 +26,8 @@ fun performPurchase(price: Double) {
     // показываем баланс после совершения покупки, округлив до 2х знаков после точки
     val remainingBalance = totalPurse - price
     println("Remaning balance: ${"%.2f".format(remainingBalance)}")
+    val remainingBalanceBarrel = barrel - PINTA
+    println("Remainder dirnk in barrel: $remainingBalanceBarrel")
 
     // разбираем обратно нашу единую валюту (типо 4.19) на золото(4) и серебро(19)
     val remainingGold = remainingBalance.toInt() // откидываем дробную часть
@@ -47,7 +55,7 @@ private fun placeOrder(menuData: String) {
     val type = data[0]
     val name = data[1]
     val price = data[2]
-    */
+     */
 
     val (type, name, price) = menuData.split(',') // деструктуризация
     val message = "Madrigal buys a $name ($type) for $price."
@@ -61,6 +69,10 @@ private fun placeOrder(menuData: String) {
         "Madrigal says: Thanks for the $name."
     }
     println(phrase)
+}
+
+private fun displayRemainderBarrel(barrel: Double) {
+    println("Remainder drink in barrel - $barrel")
 }
 
 /** Описание phrase.replace(Regex...
